4fd4d7faa16c9a503c2dacffd58fcc33438d40b3
風平浪靜 <15524261@qq.com>
2021-09-18 20:21:10 +0800

    feat：1.1.6

--- test/index.ts
+++ test/index.ts
@@ -1,36 +1,36 @@
 interface Request {
-  /** 200 成功  -200失败 **/
+  /** 200 成功  -200失败 */
   code: number;
-  /** 返回成功 **/
+  /** 返回成功 */
   message: string;
-  /** undefined **/
+  /** 注释 */
   data: Data;
 }
 
 interface Data {
-  /** 注释 **/
+  /** 注释 */
   complianceOnlineCars: ComplianceOnlineCars;
-  /** 注释 **/
+  /** 注释 */
   inCount: number;
-  /** 注释 **/
+  /** 注释 */
   outCount: number;
-  /** 注释 **/
+  /** 注释 */
   statisticsTime: string;
-  /** 注释 **/
+  /** 注释 */
   createTime: string;
 }
 
 interface ComplianceOnlineCars {
-  /** 站场名称 **/
+  /** 站场名称 */
   statName: string;
-  /** 日运行车次（车次） **/
+  /** 日运行车次（车次） */
   dailyService: number;
-  /** 日客运量（人次） **/
+  /** 日客运量（人次） */
   passengers: number;
-  /** 保点车次（车次） **/
+  /** 保点车次（车次） */
   keepOnlineCar: number;
-  /** 保点客运量（人次） **/
+  /** 保点客运量（人次） */
   keepPassengers: number;
-  /** 统计时间 **/
+  /** 统计时间 */
   statisticsTime: string;
 }
